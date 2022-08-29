128a80348dab97c007ea6f40b46830920d925972
karenan1811 <89387334+karenan1811@users.noreply.github.com>
2022-08-29 11:48:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // type safety - tip guvenlifgi
+        // Do not repeat yourself
+        string categoryTag = "Category";
+        int studentTall = 32000;
+        double renteProocent = 1.45;
+        bool erDuLogIn = true;
+        double dollarDun = 7.55;
+        double dollarBugun = 7.55;
+
+        if (dollarDun>dollarBugun)
+        {
+            Console.WriteLine("Azalis butonu");
+        }
+        else if (dollarDun<dollarBugun)
+        {
+            Console.WriteLine("Artis butonu");
+        }
+        else
+        {
+            Console.WriteLine("Degismedi");
+        }
+
+        if (erDuLogIn == true)
+
+           
+        {
+            Console.WriteLine(" Users setnings button");
+        }
+        else
+        {
+            Console.WriteLine("Login button");
+        }
+        Console.WriteLine("categoryTag");
+    }
+}
